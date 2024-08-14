0e2769e0ea59fddf92b2be93b20863309959c53c
almajacob <almajacob7@gmail.com>
2024-08-14 13:36:56 +0530

    done

--- operators.py
+++ operators.py
@@ -1,43 +1,180 @@
 # Arithemetic_operators
 
-print(10+5) #addition
-print("alma"+"jacob")
-print(10*5) #multiplication
-print("alma:"*4)
-print(10-5) #division
-print(10%3) #modular division
-print(10/5) #division
-print(10**2) #finding square
+# print(10+5) #addition
+# print("alma"+"jacob")
+# print(10*5) #multiplication
+# print("alma:"*4)
+# print(10-5) #division
+# print(10%3) #modular division
+# print(10/5) #division
+# print(10**2) #finding square
 
-#Assignment operators
-a=5 
-print(a)  # =
-a=25 
-a+=5 # +=
-print(a)
-a=25
-a-=5 # -=
-print(a)
-a=25
-a*=5 # *=
-print(a)
-a=25
-a**=5 # **=
-print(a)
-a=25
-a/=5 # /=
-print(a)
-a=25
-a//=5 # //=
-print(a)
+# #Assignment operators
+# a=5 
+# print(a)  # =
+# a=25 
+# a+=5 # +=
+# print(a)
+# a=25
+# a-=5 # -=
+# print(a)
+# a=25
+# a*=5 # *=
+# print(a)
+# a=25
+# a**=5 # **=
+# print(a)
+# a=25
+# a/=5 # /=
+# print(a)
+# a=25
+# a//=5 # //=
+# print(a)
 
-#coparison operator
+# #coparison operator
 
-print(5==5) # ==
-print("alma"=="aalma")
-print(5!=3) # !=
-print("alma"!="alma")
-print(5<2) # <
-print(5<=2) #<=
-print(6>5) # >
-print(6>=5) # >=
+# print(5==5) # ==
+# print("alma"=="aalma")
+# print(5!=3) # !=
+# print("alma"!="alma")
+# print(5<2) # <
+# print(5<=2) #<=
+# print(6>5) # >
+# print(6>=5) # >=
+
+# logical operators
+
+#and
+
+# print(10==10 and 10>9)
+# True   True =True
+# print(10==10 and 10>10)
+#  True   False =False
+# print(10==6 and 10>5)
+#False   True =False
+# print(10==6 and 10>11)
+#False   False =False
+
+#or
+
+# print(10==10 and 10>9)
+# # True   True =True
+# print(10==10 and 10>10)
+# #True   False =True
+# print(10==6 and 10>5)
+# #False   True =True
+# print(10==6 and 10>11)
+# #False   False =False
+
+
+#not
+# 
+
+#membership operator
+
+ #in
+
+# a='alma'
+# b="d"
+# print(b in a)
+#false
+# a='alma'
+# b="m"
+# print(b in a)
+# #True
+
+#not in 
+
+# a="heaven"
+# f="j"
+# print( f not in "heaven")
+#True
+# a="heaven"
+# f="a"
+# print( f not in "heaven")
+#False
+
+# idnetitiy operator
+
+# is
+
+# a=10
+# b=3
+# print(a is b)
+#False
+
+# a=10
+# b=10
+# print(a is b)
+#True
+
+# is in the case of list
+
+# l=[2,4,9]
+# k=[2,3,6]
+# print(id(l))
+# print(id(k))
+# print( l in k)
+#false
+
+
+# l=[2,4,9]
+# k=[2,4,9]
+# print(id(l))
+# print(id(k))
+# print( l in k)
+#false
+
+#is in the case of string
+
+# l="anu"
+# k='aju'
+# print(l is k)
+# print(l is not k)
+#False
+#True
+
+# l="anu"
+# k='anu'
+# print(l is k)
+# print(l is not k)
+#True
+#False
+
+# equal to in the case of string
+r
+# l="anu"
+# k='aju'
+# print(l == k)
+# print(l != k)
+#False
+#True
+
+# l="anu"
+# k='anu'
+# print(l == k)
+# print(l != k)
+#True
+#False
+
+
+
+
+
+
+#not is
+
+# a=20
+# b=10
+# print(a is not b)
+# True
+
+# a=20
+# b=20
+# print(a is not b)
+#False
+
+
+
+
+
---
+++ string.py
@@ -0,0 +1,8 @@
+a="python"
+print(type(a))
+b="123"
+print(type(b))
+c="[1,2,3]"
+print(type(c))
+print('''pyth
+      on''')
