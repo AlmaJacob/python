6712b259cfa231af2d86dee487dcdef89874928f
almajacob <almajacob7@gmail.com>
2024-08-15 13:38:34 +0530

    done

--- string.py
+++ string.py
@@ -1,8 +1,67 @@
-a="python"
-print(type(a))
-b="123"
-print(type(b))
-c="[1,2,3]"
-print(type(c))
-print('''pyth
-      on''')
+# a="python"
+# print(type(a))
+# b="123"
+# print(type(b))
+# c="[1,2,3]"
+# print(type(c))
+# print('''pyth
+#       on''')
+
+# slicing
+
+# a="hope"
+# print(a[0:2])
+# a="life"
+# print(a[-4:-1])
+
+#print(a[starting_index:ending_index)
+
+
+#finding reverse of a string using slicing
+
+# a="human"
+# print(a[::-1])
+
+#print(a[starting_index:ending_index:step])
+
+# 
+# a=input("enter a string")
+# print(a[::3])
+# print(a[::-1])
+# print(a==a[::-1])
+
+# string methods
+
+a="lifE of joy"
+print(a.upper())
+print(a.lower())
+print(a.capitalize())
+print(a.count('o'))
+print(a.find("f"))
+print(a.endswith("y"))
+print(a.endswith("j"))
+print(a.startswith("k"))
+print(a.startswith("l"))
+print(a.center(6))
+b="     everbody    "
+print(b.strip(),'*')
+print(b.lstrip(),'*')
+print(b.rstrip(),'*')
+print(b.isupper())
+print(b.islower())
+c="235"
+print(c.isdigit())
+d="fgh,3,k"
+print(d.isdigit())
+
+
+# formatting table
+
+print("{:12}{:12}{:6}".format('name','sub','class'))
+print("{:12}{:12}{:6}".format('alma','malyalalam','10th'))
+print("{:12}{:12}{:6}".format('anu','eng','12th'))
+print("{:12}{:12}{:6}".format('aju','hindi','5th'))
+print("{:12}{:12}{:6}".format('ashi','urdu','9th'))
+
+
+
