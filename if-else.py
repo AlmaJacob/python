8bbba93a1026e7789c03c939a0c441f828a18080
almajacob <almajacob7@gmail.com>
2024-08-16 13:37:06 +0530

    done

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# if cond:
+#     print(stm)
+# else:
+#     print(stm)
+
+# a="colour"
+# b="black"
+# if a==b:
+#     print("true")
+# else:
+#     print('false')
+
+# 
+
+# 
---
+++ task.py
@@ -0,0 +1,23 @@
+# num=int(input("enter a number"))
+# if num%2==0:
+#     print(num,"is even")
+# else:
+#     print(num,"is odd")
+
+# a=(input("enter a string"))
+# b=(input("enter a letter"))
+# if (b in a):
+#         print("b is in a")
+# else:
+#         print("b is not in a")
+
+
+# 
+      
+digit=int(input("enter a number"))
+l=%10
+
+
+       print("these digit is divisible by 3")
+else:
+        print("these digit is divisible by 3")
