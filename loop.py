ee2b902ed5eb1f98a59b6760f5c89f0af5e87b5f
almajacob <almajacob7@gmail.com>
2024-08-20 13:27:07 +0530

    done

--- loop.py
+++ loop.py
@@ -80,20 +80,55 @@
 #     print(a,'*',b,"=",a*b)
 #     a+=1
 
-a=int(input("enter starting number "))
-b=int(input('enter last number'))
-n=0
-even=0
-odd=0
-while a<=b:
-    n=n+a
-    if a%2==0:
-        even=even+a
-    else:
-        odd=odd+a
-    a+=1    
-print("sum of n natural numbers :  ",n) 
-print("sum of even  numbers :  ",even)    
-print("sum of odd numbers :  ",odd)    
-       
+# a=int(input("enter starting number "))
+# b=int(input('enter last number'))
+# n=0
+# even=0
+# odd=0
+# while a<=b:
+#     n=n+a
+#     if a%2==0:
+#         even=even+a
+#     else:
+#         odd=odd+a
+#     a+=1    
+# print("sum of n natural numbers :  ",n) 
+# print("sum of even  numbers :  ",even)    
+# print("sum of odd numbers :  ",odd)    
+
+
+#factorial of a number
+
+# b=int(input("enter a limit"))
+# fact=1
+# a=1
+
+# while a<=b:
+#     fact=fact*a
+   
+#     a+=1
+# print("fact ",fact)
+
+#reverse of a number
+
+# j=int(input("enter a number"))
+# i=1
+# rev=0
+# while j>0:
+#     d=j%10
+#     rev=rev+d
+#     j//=10
+# print(rev)
+   
+#print string top to bottum using while loop
+
+j=input("enter a string")
+l=len(j)
+i=0
+rev=""
+while i<l:
+    rev=j[i]+rev
     
+    i+=1
+print(rev)    
+
