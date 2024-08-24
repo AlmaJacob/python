91750b580aab97499b9d14b8367b483ce1ae87d6
AlmaJacob <almajacob7@gmail.com>
2024-08-24 13:36:29 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,70 @@
+#5*4 matrix
+
+# for i in range(4):
+#     print()
+# for j in range(5):
+#         print(j,end="\t")
+
+#3*3 matrix 
+
+# for i in range(3):
+#       a="alma"
+#       print()
+#       for j in range(3):
+#             a+="jacob"
+#             print(a,end="\t")
+            
+            
+        
+# for i in range(3):
+   
+
+#  for j in range(3):
+      
+#   print(i,end="\t")
+#  print()
+
+"""0 0 0
+   1 1 1
+   2 2 2
+"""
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="\t")
+#         i+=1
+#     print()
+"""0 1 2
+       1 2 3
+       2 3 4 """
+
+# a=0     
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="\t")
+#         a+=1 
+#     print()
+        
+"""0 1 2
+   3 4 5
+   6 7 8"""
+
+
+
+# for i in range(4):
+#     if i%2==0:
+#         for j in range(3):
+#           print(j,end="\t")
+#     else:
+#          for j in range(3):
+#             print(2-j,end="\t")
+#     print()  
+
+"""0 1 2
+       2 1 0
+       0 1 2
+       2 1 0"""
+
+        
+
