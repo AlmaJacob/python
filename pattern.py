812558fe05fe031653445b943eb6f730ce143153
AlmaJacob <almajacob7@gmail.com>
2024-08-27 13:53:45 +0530

    pattern update

--- pattern.py
+++ pattern.py
@@ -1,17 +1,19 @@
 #5*4 matrix
 
 # for i in range(4):
-#     print()
+#    print()
 # for j in range(5):
 #         print(j,end="\t")
+"""0 1  2 3 4"""
+
 
 #3*3 matrix 
 
 # for i in range(3):
-#       a="alma"
+#       a=1
 #       print()
 #       for j in range(3):
-#             a+="jacob"
+#             a+=1
 #             print(a,end="\t")
             
             
@@ -67,4 +69,116 @@
        2 1 0"""
 
         
+# for i in range(3):
+#     print()
+#     a=65
+#     for j in range(3):
+#       print(chr(a+j),end=" ")
+# print()
+
+"""A B C
+   A B C
+   A B C"""
+
+# a=1
+# for i in range(1,5):
+#    for j in range(i):
+#       print(a,end=" ")
+#       a+=1
+#    print()  
+
+"""1
+   2 3
+   4 5 6
+   7 8 9 10""" 
+
+# for i in range(1,4):
+#       for j in range(i):
+#          print(i-j,end=" ")
+#       print()     
+
+"""1
+   2 1
+   3 2 1"""
+
+# a=1
+# for i in range(3):
+#      for j in range(3):
+#           print(a,end=" ")
+#           a+=2
+#           print()    
+"""1 3 5
+   7 9 11
+   13 15 17"""
+
+# for i in range(3):
+#    for j in range(3):
+#          if i==j:
+#            print(5,end=" ")
+#          else:
+#            print("#",end=" ")
+#    print()
+
+"""5 # #
+   # 5 #
+   # # 5"""
+
+# for i in range(1,4):
+#       a=65
+#       for j in range(i):
+#          print(chr(a+j),end=" ")
+#       print() 
+
+"""A 
+   A B
+   A B C"""
+
+# for i in range(1,4):
+#       a=64
+#       for j in range(i):
+#          print(chr(a+i),end=" ")
+         
+#       print()    
+"""A
+   B B
+   C C C""" 
+
+# for i in range(1,4):
+#       a=64
+#       for j in range(i):
+#          print(chr(a+i),end=" ")
+#          a-=1
+         
+#       print()     
+
+"""A 
+   B A
+   C B A"""
+
+
+# for i in range(4):
+#     if i%2==0:
+#         for j in range(3):
+#           print(j,end="\t")
+#     else:
+#          a=65
+#          for j in range(3):
+#             print(chr(a+ j),end="\t")
+#     print()  
+
+
+"""0 1 2
+   A B C
+   0 1 2
+   A B C"""
+
+# l=[2,6,4,1]
+# for i in l:
+#     print("*"*i)
+
+"""**
+   ******
+   ****
+   *"""
+
 
