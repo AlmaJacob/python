ba9cfce155b498a94c3bd6f39eea3ef74aa1c590
almajacob <almajacob7@gmail.com>
2024-08-29 13:38:03 +0530

    done

--- list.py
+++ list.py
@@ -110,7 +110,92 @@
 #index(to check the index of a element in list)
 #------------------------------------------------------
 
-l=[5,2,8,4,14]
-print(l.index(4
-              ))
+#task
+#-------
+#sum of even number is sum
+#------------------------------
 
+# l=[5,2,8,4,14]
+# sum=0
+# for i in l:
+#     if i%2==0:
+#         sum+=i
+#         print(i)
+# print(sum)        
+
+#print the reverse of string in list of each element
+#--------------------------------------------------------
+
+# l=['alma','origin','name']
+# for i in l:
+#     print(i[::-1])
+
+
+#if the elements in the list is float or int then print the sum
+#------------------------------------------------------------------
+
+# l=[1,2,10.3,5.8,'ajay']
+# sum=0
+# for i in l:
+#     if type(i)==int or type(i)==float:
+#         sum+=i
+# print(sum)
+
+
+#if any duplicate element is present in list print non-duplicate elements
+#----------------------------------------------------------------------------
+
+# l=[1,34,1,100,4,34]
+# l1=[]
+# for i in l:
+#   if i not in l1:
+#     l1.append(i)
+# print(l1)
+
+#taking name by inputing and print in list
+#-------------------------------------------
+
+# names=[]
+# limit=int(input("enter a limit"))
+# for i in range(limit):
+#     name=input("enter name")
+#     names.append(name)
+# print(names)
+
+#print the details of a student in list using nested list
+#---------------------------------------------------------
+
+# std=[]
+# limit=int(input("enter a limit"))
+# for i in range(limit):
+#     name=input("enter name")
+#     age=input("enter age") 
+#     mark=input("enter mark")
+#     std.append([name,age,mark])
+# print(std)
+
+
+#we want to add,delete,update,view the list
+#-------------------------------------------------
+
+# std=[]
+# while True:
+#         print(""" 
+# 1.add std
+# 2.view std
+# 3.update std
+# 4.delete std
+#              """)
+#         choice=int(input("enter ur choice"))
+#         if choice==1:
+#                name=input("enter name")
+#                age=input("enter age") 
+#                mark=input("enter mark")
+#                std.append([name,age,mark])
+#         elif choice==2:
+#               print('{:<10}{:<6}{:<6}'.format("name","age","mark"))
+#               for i in std:
+#                       print('{:<10}{:<6}{:<6}'.format(i[0],i[1],i[2]))
+                    
+              
+                     
