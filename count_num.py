b49c0913839a5f0d9b9eca571b20abe949194d38
Almajacob <almajacob7@gmail.com>
2024-09-25 13:40:45 +0530

    done

---
+++ count_num.py
@@ -0,0 +1,37 @@
+
+# counting numbers in a text file
+# f=open('python/demo2.txt','r')
+# c=f.readlines()
+# f.seek(0)
+# letter=0
+# for i in c:
+#     e=f.readline().strip()
+#     for i in e:
+#         if i!= " ":
+#             letter+=1
+# print(letter)
+
+#counting capital and small letter in text file
+f=open('python/demo2.txt','r')
+l=f.readlines()
+f.seek(0)
+cap=0
+letters=0
+words=0
+for i in range(len(l)):
+    a=f.readline().strip()
+    s=a.split()
+    for i in s:
+        if i!='':
+            words+=1
+    for i in a:
+        if i!=' ':
+            if i.isupper():
+                cap+=1
+            letters+=1
+print(letters)
+print('capital',cap)
+print('small',letters-cap)
+print('words',words)
+
+
--- multiplication_table.py
+++ multiplication_table.py
@@ -1,7 +1,14 @@
-f=open("python/mul_table1.txt",'w')
-b=int(input("enter a number"))
-a=1
-while a<=10:
-    print(a,'*',b,"=",a*b)
-    f.write(str(a)+'*'+str(b)+'='+str(a*b)+"\n")   
-    a+=1
+
+# b=int(input("enter a number"))
+# a=1
+# while a<=10:
+#     print(a,'*',b,"=",a*b)
+#     f.write(str(a)+'*'+str(b)+'='+str(a*b)+"\n")   
+#     a+=1
+f=open("mul_table1.txt",'w')
+a=int(input("enter a number"))
+for i in range(1,11):
+    for j in range(1,a+1):
+         f.write(str(i)+'*'+str(j)+'='+str(j*i)+"\t")
+    f.write("\n")        
+
