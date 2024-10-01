315f0830d6b24929dfaef867b51781efd4173321
almajacob <almajacob7@gmail.com>
2024-10-01 13:28:47 +0530

    task

--- count_num.py
+++ count_num.py
@@ -12,24 +12,24 @@
 # print(letter)
 
 #counting capital and small letter in text file
-f=open('python/demo2.txt','r')
-l=f.readlines()
-f.seek(0)
-cap=0
-letters=0
-words=0
-for i in range(len(l)):
-    a=f.readline().strip()
-    s=a.split()
-    for i in s:
-        if i!='':
-            words+=1
-    for i in a:
-        if i!=' ':
-            if i.isupper():
-                cap+=1
-            letters+=1
-print(letters)
-print('capital',cap)
-print('small',letters-cap)
-print('words',words)
+# f=open('python/demo2.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# cap=0
+# letters=0
+# words=0
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     s=a.split()
+#     for i in s:
+#         if i!='':
+#             words+=1
+#     for i in a:
+#         if i!=' ':
+#             if i.isupper():
+#                 cap+=1
+#             letters+=1
+# print(letters)
+# print('capital',cap)
+# print('small',letters-cap)
+# print('words',words)
