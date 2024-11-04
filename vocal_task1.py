bc615831638020e210615ba1c2330f71d863b893
almajacob <almajacob7@gmail.com>
2024-11-04 13:28:25 +0530

    added

---
+++ vocal_task1.py
@@ -0,0 +1,29 @@
+#reverse of a string using while loop
+
+# j=input("enter a string")
+# l=len(j)
+# i=0
+# rev=""
+# while i<l:
+#     rev=j[i]+rev
+    
+#     i+=1
+# print(rev)  
+
+#finding largest  string
+# l=['apple','kiwi','orange']
+# longest=l[0]
+# for i in l:
+#     if len(i)>len(longest):
+#         longest=i
+#         print(longest)
+
+#finding largest value from a list
+# l=[1,2,3,4,6]
+# largest=l[0]
+
+# for i in l:
+#     if i>largest:
+#         largest=i
+# print(largest)
+
